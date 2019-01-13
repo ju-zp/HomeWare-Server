002b0cc787a458c8af6679703f4b0fd1f12ebe04
ju-zp <jhuntington@tutanota.com>
2019-01-13 14:29:20 +0000

    added functionality to signup a user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,12 @@ class UsersController < ApplicationController
 
     def login
         puts 'login'
+        @user = User.new(username:params[:username], password:params[:password])
+        if @user.save
+            render json: {username: @user.username, id: @user.id}
+        else
+            render json: {error: 'Invalid'}, status: 401
+        end
     end
 
     def signup
