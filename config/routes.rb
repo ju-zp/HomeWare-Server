16eb33282be519ff4bda4d1e6d089d45fdafb2bf
ju-zp <jhuntington@tutanota.com>
2019-01-14 11:15:43 +0000

    added route to temperature controller to handle reading

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #user routes
   post 'login', to: 'users#login'
   post 'signup', to: 'users#signup'
   post 'validate', to: 'users#validate'
   post 'logout', to: 'users#logout'
+
+  #temperature routes
+  post 'reading', to: 'temperatures#reading'
 end
