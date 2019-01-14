e426fa59f40be321987a46dedbfe0de9fc5f5068
ju-zp <jhuntington@tutanota.com>
2019-01-14 11:59:46 +0000

    added response from server

--- app/controllers/temperatures_controller.rb
+++ app/controllers/temperatures_controller.rb
@@ -4,7 +4,9 @@ class TemperaturesController < ApplicationController
         @session = User.find_by(username: params[:username]).sessions.last
         if @session
             @tempReading = TemperatureReading.create(session: @session, reading: params[:reading], time: DateTime.current())
-            puts @tempReading
+            render json: {success: 'successfully recorded temperature'}, status: 201
+        else
+            render json: {error: 'Unable to record temperature'}, status: 501
         end
         
     end
