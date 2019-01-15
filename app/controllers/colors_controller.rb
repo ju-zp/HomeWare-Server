22d59a0ac9b886fcc917ca87717d32f4a374b3a7
ju-zp <jhuntington@tutanota.com>
2019-01-15 09:07:23 +0000

    added function to handle color params and create color obj

--- app/controllers/colors_controller.rb
+++ app/controllers/colors_controller.rb
@@ -1,7 +1,15 @@
 class ColorsController < ApplicationController
 
     def saveColor
+        @color = Color.new(color_params)
+        byebug
         puts "hello"
+    end
+
+    private 
+
+    def color_params
+        params.require(:color).permit(:red, :green, :blue)
     end
 
 end
