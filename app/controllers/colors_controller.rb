e7f068c5cf9726274ac7565234a16643ec00fff1
ju-zp <jhuntington@tutanota.com>
2019-01-15 09:33:02 +0000

    added response from server

--- app/controllers/colors_controller.rb
+++ app/controllers/colors_controller.rb
@@ -3,8 +3,12 @@ class ColorsController < ApplicationController
     def saveColor
         @user = User.find_by(username: params[:username])
         @color = Color.new(color_params)
-        byebug
-        puts "hello"
+        @color.user = @user
+        if @color.save
+            render json: {response: 'Successfully save color'}, status: 201
+        else
+            render json: {error: 'Unable to save color'}, status: 501
+        end
     end
 
     private 
