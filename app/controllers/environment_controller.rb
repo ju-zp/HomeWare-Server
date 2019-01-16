e1d06ddc135cfb61b386ffef56e2a0ecd799e407
ju-zp <jhuntington@tutanota.com>
2019-01-16 14:54:39 +0000

    changes

--- app/controllers/environment_controller.rb
+++ app/controllers/environment_controller.rb
@@ -1,6 +1,9 @@
 class EnvironmentController < ApplicationController
 
     def getEnvironment
+        @user = User.find_by(username: params[:username])
+        # @home = Home.find(@user.home_id)
+        byebug
         puts 'hello'
     end
 
