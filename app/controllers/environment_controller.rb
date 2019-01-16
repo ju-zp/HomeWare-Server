99370159cac0c7258b986096e64eb58bbd76ad78
ju-zp <jhuntington@tutanota.com>
2019-01-16 15:00:59 +0000

    updated end point to send home and board data

--- app/controllers/environment_controller.rb
+++ app/controllers/environment_controller.rb
@@ -2,7 +2,11 @@ class EnvironmentController < ApplicationController
 
     def getEnvironment
         @user = User.find_by(username: params[:username])
-        # @home = Home.find(@user.home_id)
+        @home = Home.find(@user.home_id)
+        if @home 
+            render json: {home: @home.name, boards: @home.boards.map(|b| b.name)}
+        else
+            render json: {error: 'No environment found'}, status: 401
         byebug
         puts 'hello'
     end
