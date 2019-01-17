edb2f9b03b6c2b820453ea76a4288b8a1a19ee11
ju-zp <jhuntington@tutanota.com>
2019-01-17 09:14:29 +0000

    updated the responce to include a hash of the board obj

--- app/controllers/environment_controller.rb
+++ app/controllers/environment_controller.rb
@@ -4,7 +4,7 @@ class EnvironmentController < ApplicationController
         @user = User.find_by(username: params[:username])
         @home = Home.find(@user.home_id)
         if @home 
-            render json: {home: @home.name, boards: @home.boards.map{|b| b.name}}
+            render json: {home: @home.name, boards: @home.boards.map{|b| {name: b.name, lights: b.lights, temp: b.temperatures}}
         else
             render json: {error: 'No environment found'}, status: 401
         end
