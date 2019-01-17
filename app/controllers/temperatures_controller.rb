2b9ff570d2a839dc51d80bbd5d15d2d729813b5a
ju-zp <jhuntington@tutanota.com>
2019-01-17 11:55:55 +0000

    updated how to create temperature readings to match that of schema

--- app/controllers/temperatures_controller.rb
+++ app/controllers/temperatures_controller.rb
@@ -4,7 +4,7 @@ class TemperaturesController < ApplicationController
         @board = User.find_by(username: params[:username]).home.boards.first
         if @board
             @temperature = Temperature.first
-            @tempReading = TemperatureReading.create(board: @board, reading: params[:reading], temperature_id: @temperature.id )
+            @tempReading = TemperatureReading.create(temperature: @temperature, reading: params[:reading], temperature_id: @temperature.id )
             render json: {success: 'successfully recorded temperature'}, status: 201
         else
             render json: {error: 'Unable to record temperature'}, status: 501
