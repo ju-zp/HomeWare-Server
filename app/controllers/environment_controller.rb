dccc252d76c642cbec02eff14c50b0b672742cee
ju-zp <jhuntington@tutanota.com>
2019-01-18 12:50:43 +0000

    refracted response to return json rather than string

--- app/controllers/environment_controller.rb
+++ app/controllers/environment_controller.rb
@@ -15,9 +15,9 @@ class EnvironmentController < ApplicationController
         require 'open-uri'
         resp = open('https://api.darksky.net/forecast/ff8f969b32a86f1e9f024852877c01e0/51.525503,-0.0822229').read
         if resp 
-            render json: {weather: resp}, status 200
+            render json: {weather: JSON.parse(resp)}, status: 200
         else
-            render json: {error: 'Unable to get weather'}, status 401
+            render json: {error: 'Unable to get weather'}, status: 401
         end
     end
 
