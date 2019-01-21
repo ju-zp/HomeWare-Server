706c496004c6cf6eb5ceda821ffe1f07df84be46
ju-zp <jhuntington@tutanota.com>
2019-01-21 09:47:55 +0000

    implemented user creation and render a response

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,7 +29,18 @@ class UsersController < ApplicationController
     end
 
     def create
-        puts 'hello'
+        @user = User.new(user_params)
+        if @user.save
+            render json: {success: 'User was successfully created'}, status: 200
+        else
+            render json: {error: 'Unable to create new user'}, status: 401
+        puts @user
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :password)
     end
     
 end
