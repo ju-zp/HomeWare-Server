260e445fdee6877f677eba857df69485bded14f3
ju-zp <jhuntington@tutanota.com>
2019-01-21 11:48:17 +0000

    added functionality to chnage state of light object

--- app/controllers/lights_controller.rb
+++ app/controllers/lights_controller.rb
@@ -1,6 +1,9 @@
 class LightsController < ApplicationController
 
     def setLight
+        @light = Light.first
+        @light.switched_on = params[:state]
+        @light.save
         puts 'hello'
     end
 
